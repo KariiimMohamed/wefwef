22740ef2ddfc68bf376205aa7f71273b5b122008
Karim <karimabdelsalam44@gmail.com>
2022-03-17 05:44:15 +0200

    New Task

---
+++ Untitled-2.js
@@ -0,0 +1,12 @@
+var car = {
+    carName : "Model : BMW",
+    carYear : "Year : 2022",
+    carColor : "color : black"
+};
+
+var arr = ["carName" , "carYear" , "carColor"] ;
+
+for (var i = 0; i < 4; i++) {
+    var x = arr[i];
+    console.log(car [x]);
+}
